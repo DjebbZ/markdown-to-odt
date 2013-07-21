046ae77fff6c0c444a8019b2a372c2d6f8d21f51
DjebbZ <khalid.jebbari@gmail.com>
2013-07-21 17:13:08 +0200

    Conversion of empty strings and text with heading and text works, with tests.

--- index.js
+++ index.js
@@ -3,17 +3,20 @@
  * Copyright (c) 2011-2013, Khalid Jebbari. (MIT Licensed)
  * https://github.com/djebbz/markdown-to-odt
  */
-
-var marked = require('marked'),
-    fs = require('fs'),
+var marked = require("marked"),
+    fs = require("fs"),
+    path = require("path"),
     md2odt = {},
     rules,
-    parser;
+    parse,
+    docTmpl,
+    createDocFromContent;
 
+// associate each content type with a function that returns an ODT string
 rules = {
     // space: ,
     // code: ,
-    heading: function(item) {
+    heading: function (item) {
         return '<text:h text:outline-level="' + item.depth + '">' +
                 item.text +
                 '</text:h>';
@@ -29,7 +32,7 @@ rules = {
     // list_end: ,
     paragraph: function (item) {
         return '<text:p>' + item.text + '</text:p>';
-    },
+    }
     // html: ,
     // text: function (item) {
     //   return '<text:p>' + item.text + '</text:p>';
@@ -37,8 +40,6 @@ rules = {
 };
 
 parse = function (src) {
-    'use strict';
-
     var lexed = marked.lexer(src),
         out = '';
 
@@ -54,7 +55,44 @@ parse = function (src) {
 
 };
 
+// The 4 empty spaces are because I'm using tabs as 4 spaces in my editor.
+docTmpl = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    "<office:document>\n" +
+    "    <office:meta></office:meta>\n" +
+    "    <office:body>{{body}}</office:body>\n" +
+    "    <office:scripts></office:scripts>\n" +
+    "    <office:settings></office:settings>\n" +
+    "    <office:styles></office:styles>\n" +
+"</office:document>";
+
+createDocFromContent = function (fileName, content, callback) {
+    var parsed, fileContent, filePath;
+
+    // Parse only non empty content
+    if (content !== "") {
+        try {
+            parsed = parse(content);
+        } catch(e) {
+            callback(e);
+        }
+    } else {
+        parsed = "";
+    }
+
+    // Put content in the right place
+    fileContent = docTmpl.replace("{{body}}", parsed);
+    fileName = path.basename(fileName) + '.xml';
+    filePath = path.join(__dirname, fileName);
+
+    // Write file to disk and proceed
+    fs.writeFile(filePath, fileContent, function (err) {
+        if (err) return callback(err);
+        callback(null, filePath);
+    });
+};
+
 md2odt.parse = parse;
 md2odt.rules = rules;
+md2odt.createDocFromContent = createDocFromContent;
 
 module.exports = md2odt;
--- test/index.js
+++ test/index.js
@@ -1,69 +1,132 @@
 var md2odt = require('../index'),
-    assert = require('assert');
+    assert = require('assert'),
+    fs = require('fs'),
+    path = require('path');
 
 describe('Markdown to odt', function () {
 
     describe('parser', function () {
 
-        describe('for titles', function () {
+        it('should convert a level 1 title string', function () {
+            var src = "# Title",
+                expected = '<text:h text:outline-level="1">Title</text:h>';
 
-            it('should convert a level 1 title string', function () {
-                var src = "# Title",
-                    expected = '<text:h text:outline-level="1">Title</text:h>';
+            assert.equal(md2odt.parse(src), expected);
+        });
 
-                assert.equal(md2odt.parse(src), expected);
-            });
+        it('should convert a level 2 title string', function () {
+            var src = "## Title",
+                expected = '<text:h text:outline-level="2">Title</text:h>';
 
-            it('should convert a level 2 title string', function () {
-                var src = "## Title",
-                    expected = '<text:h text:outline-level="2">Title</text:h>';
+            assert.equal(md2odt.parse(src), expected);
+        });
 
-                assert.equal(md2odt.parse(src), expected);
-            });
+        it('should convert a level 3 title string', function () {
+            var src = "### Title",
+                expected = '<text:h text:outline-level="3">Title</text:h>';
 
-            it('should convert a level 3 title string', function () {
-                var src = "### Title",
-                    expected = '<text:h text:outline-level="3">Title</text:h>';
+            assert.equal(md2odt.parse(src), expected);
+        });
 
-                assert.equal(md2odt.parse(src), expected);
-            });
+        it('should convert a level 4 title string', function () {
+            var src = "#### Title",
+                expected = '<text:h text:outline-level="4">Title</text:h>';
 
-            it('should convert a level 4 title string', function () {
-                var src = "#### Title",
-                    expected = '<text:h text:outline-level="4">Title</text:h>';
+            assert.equal(md2odt.parse(src), expected);
+        });
 
-                assert.equal(md2odt.parse(src), expected);
-            });
+        it('should convert a level 5 title string', function () {
+            var src = "##### Title",
+                expected = '<text:h text:outline-level="5">Title</text:h>';
 
-            it('should convert a level 5 title string', function () {
-                var src = "##### Title",
-                    expected = '<text:h text:outline-level="5">Title</text:h>';
+            assert.equal(md2odt.parse(src), expected);
+        });
 
-                assert.equal(md2odt.parse(src), expected);
-            });
+        it('should convert a level 6 title string', function () {
+            var src = "###### Title",
+                expected = '<text:h text:outline-level="6">Title</text:h>';
 
-            it('should convert a level 6 title string', function () {
-                var src = "###### Title",
-                    expected = '<text:h text:outline-level="6">Title</text:h>';
+            assert.equal(md2odt.parse(src), expected);
+        });
 
-                assert.equal(md2odt.parse(src), expected);
+        it('should convert a paragraph string', function () {
+
+            var src = 'Lorem ipsum Veniam in magna reprehenderit non eu.',
+                expected = '<text:p>Lorem ipsum Veniam in magna reprehenderit non eu.</text:p>';
+
+            assert.equal(md2odt.parse(src), expected);
+        });
+
+        it('should properly convert mixed content', function () {
+
+            var src = '# Title\n' +
+                        '## Subheading\n' +
+                        'Some simple text content.',
+                expected = '<text:h text:outline-level="1">Title</text:h>' +
+                            '<text:h text:outline-level="2">Subheading</text:h>' +
+                            '<text:p>Some simple text content.</text:p>';
+
+            assert.equal(md2odt.parse(src), expected);
+        });
+
+    });
+
+    describe('file creator', function () {
+
+        describe('with empty content', function () {
+
+            it('should create a properly formatted empty odt xml file', function (done) {
+
+                var content = '',
+                    filename = 'empty',
+                    expected = fs.readFileSync(__dirname + '/fixtures/empty.xml', {encoding: 'utf8'});
+
+                md2odt.createDocFromContent(filename, content, function (err, filePath) {
+                    if (err) return done(err);
+
+                    assert(fs.existsSync(filePath), "File hasn't been created");
+
+                    content = fs.readFileSync(filePath, {encoding: 'utf8'});
+                    assert.equal(content, expected, "File doesn't have empty odt content");
+
+                    // Remove created file, avoid pollution
+                    fs.unlink(filePath, function(err) {
+                        if (err) return done(err);
+                        done();
+                    });
+                });
+
             });
 
         });
 
-        describe('for paragraphs', function() {
+        describe('with basic heading and paragraph content', function() {
 
-            it('should convert a paragraph string', function() {
+            it('should create a proper odt xml file', function (done) {
 
-                var src = 'Lorem ipsum Veniam in magna reprehenderit non eu.',
-                    expected = '<text:p>Lorem ipsum Veniam in magna reprehenderit non eu.</text:p>';
+                var expected = fs.readFileSync(__dirname + '/fixtures/heading-paragraph.xml', {encoding: "utf8"}),
+                    content = "# My title\n" +
+                        "\n" +
+                        "Lorem ipsum FTW.";
 
-                assert.equal(md2odt.parse(src), expected);
+                md2odt.createDocFromContent('testFileName', content, function (err, filePath) {
+                    if (err) return done(err);
+
+                    assert(fs.existsSync(filePath), "File hasn't been created");
+
+                    content = fs.readFileSync(filePath, {encoding: 'utf8'});
+                    assert.equal(content, expected/*, "File doesn't have proper odt content"*/);
+
+                    // Remove created file, avoid pollution
+                    fs.unlink(filePath, function(err) {
+                        if (err) return done(err);
+                        done();
+                    });
+                });
+
             });
 
         });
-
-
 
     });
 
